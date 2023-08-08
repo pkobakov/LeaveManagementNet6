89ac28e32d205fce688b1e46b8fe8f5f010af1ac
Petar Kobakov <pkobakov@gmail.com>
2023-08-08 17:44:16 +0300

    Setup Create Leave Type

--- LeaveManagement.Web/Models/LeaveTypeVM.cs
+++ LeaveManagement.Web/Models/LeaveTypeVM.cs
@@ -7,8 +7,12 @@ namespace LeaveManagement.Web.Models
     {
         public int Id { get; set; }
         [Display(Name = "Leave Type Name")]
+        [Required]
+        [StringLength(12, MinimumLength = 6, ErrorMessage ="Name Should Be Between 6 And 12 Characters.")]
         public string Name { get; set; }
         [Display(Name = "Default Number Of Days")]
+        [Required]
+        [Range(1, 25, ErrorMessage ="Please Enter A Valid Number.")]
         public int DefaultDays { get; set; }
     }
 }
