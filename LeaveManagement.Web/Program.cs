bdbb066f8a3a587b7bdd718668a4b791690c793c
Petar Kobakov <pkobakov@gmail.com>
2023-08-08 16:37:32 +0300

    Added LeaveType View Model, AutoMapper, AutoMapper Congiguration file

--- LeaveManagement.Web/Program.cs
+++ LeaveManagement.Web/Program.cs
@@ -1,3 +1,4 @@
+using LeaveManagement.Web.Configuration;
 using LeaveManagement.Web.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -18,6 +19,7 @@ namespace LeaveManagement.Web
 
             builder.Services.AddDefaultIdentity<Employee>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.Services.AddAutoMapper(typeof(MapperConfig));
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
