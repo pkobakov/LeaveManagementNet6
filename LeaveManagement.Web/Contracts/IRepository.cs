8392ee34fc0ad2262e3e6da2f7ca123574ec5917
Petar Kobakov <pkobakov@gmail.com>
2023-08-10 17:19:03 +0300

    Setup Repository and Dependency Injection

---
+++ LeaveManagement.Web/Contracts/IRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace LeaveManagement.Web.Contracts
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> GetAsync(int id);
+        Task<List<T>> GetAllAsync();
+        Task<T> AddAsync(T item);
+        Task<int> GetCountAsync();
+        Task<bool> ExistsAsync(int id);   
+        Task DeleteAsync(int id);
+        Task UpdateAsync(T entity);
+
+    }
+}
---
+++ LeaveManagement.Web/Repositories/Repository.cs
@@ -0,0 +1,60 @@
+﻿using LeaveManagement.Web.Contracts;
+using LeaveManagement.Web.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeaveManagement.Web.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext context;
+
+        public Repository(ApplicationDbContext context) 
+        {
+            this.context = context;
+        }
+
+        public async Task<T> AddAsync(T item)
+        {
+            await this.context.AddAsync(item);
+            await this.context.SaveChangesAsync();  
+            return item;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var item = await GetAsync(id); 
+            this.context.Set<T>().Remove(item);
+            await this.context.SaveChangesAsync();
+            
+        }
+
+        public async Task<bool> ExistsAsync(int id)
+        {
+            var item = await GetAsync(id);
+           return item != null;    
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await this.context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetAsync(int id)
+        {
+            return await this.context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<int> GetCountAsync()
+        {
+            var list = await GetAllAsync();
+            return list.Count;
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            //this.context.Entry(entity).State = EntityState.Modified;
+            this.context.Update(entity);    
+            await this.context.SaveChangesAsync();
+        }
+    }
+}
