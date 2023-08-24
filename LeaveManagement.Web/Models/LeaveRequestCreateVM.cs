b6884685340f51b492872ded093428f9a96ca27e
Petar Kobakov <pkobakov@gmail.com>
2023-08-24 17:54:15 +0300

    SetUp Create Leave Request View

---
+++ LeaveManagement.Web/Models/LeaveRequestCreateVM.cs
@@ -0,0 +1,21 @@
+﻿using LeaveManagement.Web.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagement.Web.Models
+{
+    public class LeaveRequestCreateVM
+    {
+        [Display(Name = "Start Date")]
+        public DateTime StartDate { get; set; }
+        [Display(Name = "End Date")]
+        public DateTime EndDate { get; set; }
+        [Display(Name = "Leave Types")]
+        public int LeaveTypeId { get; set; }
+        public SelectList LeaveTypes { get; set; }
+        [Display(Name = "Request Comments")]
+        public string RequestComments { get; set; }
+    }
+}
